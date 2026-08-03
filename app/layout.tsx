d8f681e3e73f98ff9f667e67d9beaab25c5ff6f5
zuripluslola-oss <zuripluslola@gmail.com>
2026-08-03 06:52:18 -0400

    Initialize OneSignal in the BookKit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { OneSignalProvider } from "./components/OneSignalProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <OneSignalProvider />
         {children}
       </body>
     </html>
